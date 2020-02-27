300771bafe14bf0a9c4490a5d4e1eddd90b7522c
spacebestmin <hm970826@gmail.com>
2020-02-27 13:33:16 +0900

    baseurl edited

--- client/src/components/create-santa.component.js
+++ client/src/components/create-santa.component.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios'
-const baseUrl = process.env.baseURL || "http://localhost:5000";
+const baseUrl = 'https://santahelena.herokuapp.com/' || "http://localhost:5000";
 
 export default class CreateSanta extends Component{
     constructor(props){
--- client/src/components/gift-list.component.js
+++ client/src/components/gift-list.component.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
-const baseUrl = process.env.baseURL || "http://localhost:5000";
+const baseUrl = 'https://santahelena.herokuapp.com/' || "http://localhost:5000";
 
 const Gift = props => (
     <tr>
